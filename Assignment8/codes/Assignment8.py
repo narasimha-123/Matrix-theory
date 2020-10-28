a8265a661c1f4e9064803a366395a951a6efe9a4
sachinomdubey <70533204+sachinomdubey@users.noreply.github.com>
2020-10-28 14:22:33 +0530

    Add files via upload

--- Assignment8/codes/Assignment8.py
+++ Assignment8/codes/Assignment8.py
@@ -1,8 +1,7 @@
-# Python program to find the Perpendicular(shortest) 
-# distance between a point and a Plane in 3 D. 
-  
+ 
 import math 
-  
+import numpy as np
+
 # Function to find distance 
 def shortest_distance(x1, y1, z1, a, b, c, d):  
       
@@ -10,8 +9,6 @@ def shortest_distance(x1, y1, z1, a, b, c, d):
     e = (math.sqrt(a * a + b * b + c * c)) 
     return (d/e)
     
-      
-  
 # Point
 x1 = -1
 y1 = 2
@@ -25,4 +22,31 @@ d = -2
   
 # Function call 
 d=shortest_distance(x1, y1, z1, a, b, c, d)   
-print("The distance between the point and plane is", d)
+print("The distance between the point and plane using conventional formula is ", d)
+
+#SVD of M matrix
+M=np.array([[1,0],[0,1],[1/2,1/3]])
+u, s, vT = np.linalg.svd(M, full_matrices=True)
+
+#pseudo inverse of s i.e s+
+smat=np.zeros((3,2))
+smat[:2, :3] = np.diag(s)
+sinv=np.linalg.pinv(smat)
+
+#Matrix b
+b=np.array([[-2],[5/2],[-4]])
+
+#finding least square solution x=vS+U^Tb
+uTb=np.matmul(np.transpose(u),b)
+sinvuTb=np.matmul(sinv,uTb)
+x=np.matmul(np.transpose(vT),sinvuTb)
+print("\nx= ",x)
+
+#finding the foot of perpendicular
+Q=np.array([[1],[-1/2],[0]])+x[0,0]*M[:,[0]]+x[1,0]*M[:,[1]]
+print("\nFoot of perpendicular Q is \n", Q)
+
+#finding the distance from point D (-1,2,-4)
+d=np.array([[-1],[2],[-4]])
+distance=np.linalg.norm(Q-d)
+print("\nThe distance using the foot of perpendicular is \n",distance)
